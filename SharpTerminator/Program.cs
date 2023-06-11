d790eee02f61830a6294d45c48807055b740451a
Mert Daş <48562581+mertdas@users.noreply.github.com>
2023-06-11 12:39:31 +0300

    Update Program.cs

--- SharpTerminator/Program.cs
+++ SharpTerminator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -136,6 +136,20 @@ class Program
         return ecount;
     }
 
+    static bool StartService(string serviceName)
+    {
+        try
+        {
+            Process.Start("sc", $"start {serviceName}");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to start the service: " + ex.Message);
+            return false;
+        }
+    }
+
     static void Main(string[] args)
     {
         if (args.Length < 1)
@@ -161,6 +175,20 @@ class Program
         }
 
         Console.WriteLine("Driver loaded successfully!");
+
+        // Yeni eklenen servisi başlat
+        if (!StartService("Terminator"))
+        {
+            Console.WriteLine("Failed to start the service.");
+            return;
+        }
+
+        Console.WriteLine("Service started successfully!");
+
+        // Belirtilen süre kadar bekle
+        int waitTimeMilliseconds = 4000; // 4 saniye
+        Console.WriteLine("Waiting for {0} milliseconds...", waitTimeMilliseconds);
+        Thread.Sleep(waitTimeMilliseconds);
 
         IntPtr hDevice = NativeMethods.CreateFile(
             @"\\.\\ZemanaAntiMalware",
@@ -200,178 +228,178 @@ class Program
                 Thread.Sleep(700);
         }
     }
-}
 
-class NativeMethods
-{
-    public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
-
-    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr OpenSCManager(
-        string machineName,
-        string databaseName,
-        ServiceManagerAccess desiredAccess);
-
-    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr OpenService(
-        IntPtr hSCManager,
-        string lpServiceName,
-        ServiceAccess dwDesiredAccess);
-
-    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern bool CloseServiceHandle(IntPtr hSCObject);
-
-    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr CreateService(
-        IntPtr hSCManager,
-        string lpServiceName,
-        string lpDisplayName,
-        ServiceAccess dwDesiredAccess,
-        ServiceType dwServiceType,
-        ServiceStartType dwStartType,
-        ServiceErrorControl dwErrorControl,
-        string lpBinaryPathName,
-        string lpLoadOrderGroup,
-        IntPtr lpdwTagId,
-        string lpDependencies,
-        string lpServiceStartName,
-        string lpPassword);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr CreateFile(
-        string lpFileName,
-        GenericAccess dwDesiredAccess,
-        FileShare dwShareMode,
-        IntPtr lpSecurityAttributes,
-        FileMode dwCreationDisposition,
-        FileFlagsAndAttributes dwFlagsAndAttributes,
-        IntPtr hTemplateFile);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern bool DeviceIoControl(
-        IntPtr hDevice,
-        uint dwIoControlCode,
-        ref uint lpInBuffer,
-        int nInBufferSize,
-        IntPtr lpOutBuffer,
-        int nOutBufferSize,
-        out int lpBytesReturned,
-        IntPtr lpOverlapped);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern IntPtr CreateToolhelp32Snapshot(
-        SnapshotFlags dwFlags,
-        uint th32ProcessID);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern bool Process32First(
-        IntPtr hSnapshot,
-        ref PROCESSENTRY32 lppe);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern bool Process32Next(
-        IntPtr hSnapshot,
-        ref PROCESSENTRY32 lppe);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern bool CloseHandle(
-        IntPtr hObject);
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    public struct PROCESSENTRY32
+    class NativeMethods
     {
-        public uint dwSize;
-        public uint cntUsage;
-        public uint th32ProcessID;
-        public IntPtr th32DefaultHeapID;
-        public uint th32ModuleID;
-        public uint cntThreads;
-        public uint th32ParentProcessID;
-        public int pcPriClassBase;
-        public uint dwFlags;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-        public string szExeFile;
-    }
+        public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
 
-    [Flags]
-    public enum GenericAccess : uint
-    {
-        GENERIC_READ = 0x80000000,
-        GENERIC_WRITE = 0x40000000
-    }
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(
+            string machineName,
+            string databaseName,
+            ServiceManagerAccess desiredAccess);
 
-    [Flags]
-    public enum FileShare : uint
-    {
-        FILE_SHARE_READ = 0x1,
-        FILE_SHARE_WRITE = 0x2,
-        FILE_SHARE_DELETE = 0x4
-    }
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenService(
+            IntPtr hSCManager,
+            string lpServiceName,
+            ServiceAccess dwDesiredAccess);
 
-    public enum FileMode : uint
-    {
-        CREATE_NEW = 1,
-        CREATE_ALWAYS = 2,
-        OPEN_EXISTING = 3,
-        OPEN_ALWAYS = 4,
-        TRUNCATE_EXISTING = 5
-    }
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CloseServiceHandle(IntPtr hSCObject);
 
-    [Flags]
-    public enum FileFlagsAndAttributes : uint
-    {
-        FILE_ATTRIBUTE_NORMAL = 0x80,
-    }
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr CreateService(
+            IntPtr hSCManager,
+            string lpServiceName,
+            string lpDisplayName,
+            ServiceAccess dwDesiredAccess,
+            ServiceType dwServiceType,
+            ServiceStartType dwStartType,
+            ServiceErrorControl dwErrorControl,
+            string lpBinaryPathName,
+            string lpLoadOrderGroup,
+            IntPtr lpdwTagId,
+            string lpDependencies,
+            string lpServiceStartName,
+            string lpPassword);
 
-    [Flags]
-    public enum SnapshotFlags : uint
-    {
-        TH32CS_SNAPPROCESS = 0x00000002
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr CreateFile(
+            string lpFileName,
+            GenericAccess dwDesiredAccess,
+            FileShare dwShareMode,
+            IntPtr lpSecurityAttributes,
+            FileMode dwCreationDisposition,
+            FileFlagsAndAttributes dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
 
-    [Flags]
-    public enum ServiceManagerAccess : uint
-    {
-        SC_MANAGER_ALL_ACCESS = 0xF003F
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            ref uint lpInBuffer,
+            int nInBufferSize,
+            IntPtr lpOutBuffer,
+            int nOutBufferSize,
+            out int lpBytesReturned,
+            IntPtr lpOverlapped);
 
-    [Flags]
-    public enum ServiceAccess : uint
-    {
-        SERVICE_ALL_ACCESS = 0xF01FF
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr CreateToolhelp32Snapshot(
+            SnapshotFlags dwFlags,
+            uint th32ProcessID);
 
-    public enum ServiceType : uint
-    {
-        SERVICE_KERNEL_DRIVER = 0x00000001,
-        SERVICE_FILE_SYSTEM_DRIVER = 0x00000002
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool Process32First(
+            IntPtr hSnapshot,
+            ref PROCESSENTRY32 lppe);
 
-    public enum ServiceStartType : uint
-    {
-        SERVICE_BOOT_START = 0x00000000,
-        SERVICE_SYSTEM_START = 0x00000001,
-        SERVICE_AUTO_START = 0x00000002,
-        SERVICE_DEMAND_START = 0x00000003,
-        SERVICE_DISABLED = 0x00000004
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool Process32Next(
+            IntPtr hSnapshot,
+            ref PROCESSENTRY32 lppe);
 
-    public enum ServiceErrorControl : uint
-    {
-        SERVICE_ERROR_IGNORE = 0x00000000,
-        SERVICE_ERROR_NORMAL = 0x00000001,
-        SERVICE_ERROR_SEVERE = 0x00000002,
-        SERVICE_ERROR_CRITICAL = 0x00000003
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CloseHandle(
+            IntPtr hObject);
 
-    public enum ServiceState : uint
-    {
-        SERVICE_STOPPED = 0x00000001,
-        SERVICE_START_PENDING = 0x00000002,
-        SERVICE_STOP_PENDING = 0x00000003,
-        SERVICE_RUNNING = 0x00000004,
-        SERVICE_CONTINUE_PENDING = 0x00000005,
-        SERVICE_PAUSE_PENDING = 0x00000006,
-        SERVICE_PAUSED = 0x00000007
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+
+        [Flags]
+        public enum GenericAccess : uint
+        {
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000
+        }
+
+        [Flags]
+        public enum FileShare : uint
+        {
+            FILE_SHARE_READ = 0x1,
+            FILE_SHARE_WRITE = 0x2,
+            FILE_SHARE_DELETE = 0x4
+        }
+
+        public enum FileMode : uint
+        {
+            CREATE_NEW = 1,
+            CREATE_ALWAYS = 2,
+            OPEN_EXISTING = 3,
+            OPEN_ALWAYS = 4,
+            TRUNCATE_EXISTING = 5
+        }
+
+        [Flags]
+        public enum FileFlagsAndAttributes : uint
+        {
+            FILE_ATTRIBUTE_NORMAL = 0x80,
+        }
+
+        [Flags]
+        public enum SnapshotFlags : uint
+        {
+            TH32CS_SNAPPROCESS = 0x00000002
+        }
+
+        [Flags]
+        public enum ServiceManagerAccess : uint
+        {
+            SC_MANAGER_ALL_ACCESS = 0xF003F
+        }
+
+        [Flags]
+        public enum ServiceAccess : uint
+        {
+            SERVICE_ALL_ACCESS = 0xF01FF
+        }
+
+        public enum ServiceType : uint
+        {
+            SERVICE_KERNEL_DRIVER = 0x00000001,
+            SERVICE_FILE_SYSTEM_DRIVER = 0x00000002
+        }
+
+        public enum ServiceStartType : uint
+        {
+            SERVICE_BOOT_START = 0x00000000,
+            SERVICE_SYSTEM_START = 0x00000001,
+            SERVICE_AUTO_START = 0x00000002,
+            SERVICE_DEMAND_START = 0x00000003,
+            SERVICE_DISABLED = 0x00000004
+        }
+
+        public enum ServiceErrorControl : uint
+        {
+            SERVICE_ERROR_IGNORE = 0x00000000,
+            SERVICE_ERROR_NORMAL = 0x00000001,
+            SERVICE_ERROR_SEVERE = 0x00000002,
+            SERVICE_ERROR_CRITICAL = 0x00000003
+        }
+
+        public enum ServiceState : uint
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007
+        }
     }
 }
